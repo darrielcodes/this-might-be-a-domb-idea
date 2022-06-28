a92905590b3858ee5bb730539cfb90335925dd64
darrielcodes <darriel.morris@codeimmersives.comgit config --global user.email darrielmorris@yahoo.com>
2022-06-28 08:24:12 -0700

    Complete assignment with html changes using JS

---
+++ main.js
@@ -0,0 +1,23 @@
+let paragraph = document.querySelector('p');
+paragraph.style.color = 'lightblue';
+paragraph.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress."
+
+let heading = document.querySelector('h1');
+heading.style.fontSize = '48px';
+
+let item13 = document.querySelector('#item-13');
+item13.style.color = 'rgba(0, 0, 0, 0.5)'
+
+let item3 = document.querySelector('#item-3');
+item3.innerText = "I say, ''Hi!''";
+
+let img = document.querySelector('img');
+img.src = "https://images.unsplash.com/photo-1563467743682-704cc8ccb9c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJldHR5JTIwZmxvd2Vyc3xlbnwwfHwwfHw%3D&w=1000&q=80"
+img.style.height = "300px"
+
+let img2 = document.querySelector('#image-2');
+img2.style.height = "300px"
+
+let item16 = document.querySelector('#item-16');
+item16.className = "item"
+item16.innerText = "Won't get fooled again."
